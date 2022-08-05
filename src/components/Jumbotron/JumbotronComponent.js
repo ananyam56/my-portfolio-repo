68468bf365553931b2c9af1646fa599abda6ca05
Ananya Maddali <ananyasai693@gmail.com>
2022-08-05 09:10:23 -0500

    Finished up basic styling of the hero

--- src/components/Jumbotron/JumbotronComponent.js
+++ src/components/Jumbotron/JumbotronComponent.js
@@ -1,16 +1,27 @@
 import * as React from 'react';
 import { StaticImage } from "gatsby-plugin-image"
+import "./Jumbotron.css"
 
 const Jumbotron = () => (
-    <div style={{display: "grid"}}>
+    <div class="jumbotron_div">
         <StaticImage 
+        src="C:\Users\shant\portfolio\gatsby-starter-default\src\components\Jumbotron\back.avif"
         style={{
             gridArea: "1/1",
+            maxHeight: 900,
         }}
-        layout="fullwidth"
-        formats={["auto", "webp", "avif"]}
-        src="C:\Users\shant\portfolio\gatsby-starter-default\src\images\filler-backdrop.jpeg"
+        layout="fullWidth"
         />
+        <div
+        style={{
+          gridArea: "1/1",
+          position: "relative",
+          placeItems: "center",
+          display: "grid",
+        }}>
+        <h1 class="jumbotron_title">Hi, I'm Ananya</h1>
+        <h2 class="jumbotron_subtitle">I'm a sophomore at Texas A&M University majoring in Computer Science!</h2>
+        </div>
     </div>
 )
 
--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,16 +1,11 @@
 import * as React from 'react';
-import Button from '@mui/material/Button';
 import Jumbotron from '../components/Jumbotron/JumbotronComponent';
 
-function App() {
-  return <Button variant="contained">Hello World</Button>;
-}
 
 export default function HomePage() {
     return (
         <div>
             <Jumbotron></Jumbotron>
-            {App()}
         </div>
     )
 }
