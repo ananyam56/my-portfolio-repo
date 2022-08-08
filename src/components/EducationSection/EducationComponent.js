ae53ab5207fd6cf4bedda6781eb50c29ec7b5ee3
Ananya Maddali <ananyasai693@gmail.com>
2022-08-08 18:22:59 -0500

    changes to sections

--- src/components/EducationSection/EducationComponent.js
+++ src/components/EducationSection/EducationComponent.js
@@ -1,17 +1,11 @@
 import * as React from 'react'
 import "./Education.css"
-import { StaticImage } from 'gatsby-plugin-image'
 
 const EducationSection = () => (
     <div>
     <div class="education_section">
-        <div class="education_image">
-            <StaticImage className = {"texasam_image"}
-            src="C:\Users\shant\portfolio\gatsby-starter-default\src\components\EducationSection\texasam.avif"
-            ></StaticImage>
-        </div>
         <div class="education_content">
-            <h1 class="education_title">My Education</h1>
+            <h1 class="education_title">My Experience</h1>
                 <ul class="education_bullets">
                     <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li> 
                     <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
