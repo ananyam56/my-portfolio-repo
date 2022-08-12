5aa1c2439bcf1a95000ba70fe99871cf59e8f2d7
Ananya Maddali <ananyasai693@gmail.com>
2022-08-12 15:25:45 -0500

    adding more projects and links

--- src/components/Card/CardComponent.js
+++ src/components/Card/CardComponent.js
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import "./Card.css"
-import {Card} from '@mui/material'
+import {Card, CardActionArea} from '@mui/material'
 import cardcontent from './CardContent'
 import {Divider} from '@mui/material'
 
@@ -11,10 +11,15 @@ export const CardComponent = cardcontent.map((card) =>
     className={"card_style"}
     id={card.id}
     >
+        <CardActionArea href={card.link}
+        target="_blank">
         {card.image}
+        </CardActionArea>
         <Divider></Divider>
         <h1 class="card_title">{card.title}</h1>
         <span class="card_date"><i>{card.date}</i></span>
         <p class="card_content">{card.content}</p>
+        <a href={card.icon1link} target="_blank" rel="noreferrer">{card.icon1}</a>
+        <a href={card.icon2link} target="_blank" rel="noreferrer">{card.icon2}</a>
     </Card>
 )
--- src/components/Card/CardContent.js
+++ src/components/Card/CardContent.js
@@ -1,5 +1,7 @@
 import { StaticImage } from "gatsby-plugin-image"
 import * as React from 'react'
+import slides from "./slides.pdf"
+import { Icon } from '@iconify/react';
 
 const cardcontent = [
     {
@@ -15,13 +17,46 @@ const cardcontent = [
         date: "July - August 2020",
         content: "I have programming experience utilizing linear regression, natural language processing, and neural networks through this summer workshop. The final project involved combining criminal justice and AI to predict the probability of defendants recommitting crimes; analyzed models involving Random Forests, Logistic Regression, and SQL ",
         image: <StaticImage src="./Capture1.png" className="project_image"></StaticImage>,
+        link: slides,
     },
     {
         id: 3,
         title: "Spruce Technology Website",
         date: "June - August 2022",
-        content: "I worked on a few components for the Spruce Technology website https://www.sprucetech.com/ through my internship. I used React, JavaScript, HTML, and CSS for these modules.",
+        content: "I worked on a few components for the Spruce Technology website through my internship. I used React, JavaScript, HTML, CSS, and Divi for these modules.",
         image: <StaticImage  src="./Capture2.jpg" className = "project_image"></StaticImage>,
+        icon1: <Icon className="project_icon" icon="bxl:invision" color="#f4a733" width="35" height="35" />,
+        icon2: <Icon className="project_icon" icon="la:stripe-s" color="#f4a733" width="35" height="35" />,
+        icon1link: "https://projects.invisionapp.com/d/main?origin=v7#/projects/prototypes/21353026",
+        icon2link: "https://www.sprucetech.com/",
+    },
+    {
+        id: 4,
+        title: "Training Website - News Blog",
+        date: "June - August 2022",
+        content: "I used WordPress, Divi, CSS, and HTML for this website.",
+        image: <StaticImage src="./Capture3.PNG" className = "project_image"></StaticImage>
+    },
+    {
+        id: 5,
+        title: "Real Estate Website",
+        date: "June - August 2022",
+        content: "I used WordPress, HTML, and CSS for this website.",
+        image: <StaticImage src="./realestatewebsite.PNG" className="project_image"></StaticImage>
+    },
+    {
+        id: 6,
+        title: "Blog Website",
+        date: "June - August 2022",
+        content: "I used WordPress, HTML, and CSS for this website.",
+        image: <StaticImage src="./blogwebsite.PNG" className="project_image"></StaticImage>
+    },
+    {
+        id: 7,
+        title: "Corporate Website",
+        date: "June - August 2022",
+        content: "I used WordPress, HTML, and CSS for this website.",
+        image: <StaticImage src="./corporatewebsite.PNG" className="project_image"></StaticImage>
     }
 ]
 
