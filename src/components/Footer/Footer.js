880c92856a53550b6bc46493caa01e5fd613d58a
Ananya Maddali <ananyasai693@gmail.com>
2022-08-16 11:20:50 -0500

    solving header and footer image problem

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,7 +8,7 @@ const FooterComponent = () => (
     <div class="footer_container">
         <div class="footer_logo_container">
         <StaticImage
-        src="../Header/Ananya Maddali-1.png"></StaticImage>
+        src="../../images/Ananya Maddali-1.png"></StaticImage>
         </div>
         <div  class="footer_sections">
         <h1 class="header_item"><a href="#section1">About Me</a></h1>
