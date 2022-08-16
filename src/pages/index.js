27eca92e5f3ff585dadf7cf4e5bba278ddb697fe
Ananya Maddali <ananyasai693@gmail.com>
2022-08-16 10:35:23 -0500

    deleting index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,129 +1,48 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import * as React from 'react';
+import Jumbotron from '../components/Jumbotron/JumbotronComponent';
+import {CardComponent} from '../components/Card/CardComponent';
+import ExperienceSection from '../components/ExperienceSection/EducationComponent';
+import {SkillsComponent} from '../components/SkillsGraph/SkillsComponent';
+import { ContactCard } from '../components/Contact/Contact';
+import AboutMeComponent from '../components/AboutMe/AboutMeComponent';
+import { StaticImage } from 'gatsby-plugin-image';
+import FooterComponent from '../components/Footer/footer';
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
-import * as styles from "../components/index.module.css"
-
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-  },
-  {
-    text: "Examples",
-    url: "https://github.com/gatsbyjs/gatsby/tree/master/examples",
-    description:
-      "A collection of websites ranging from very basic to complex/complete that illustrate how to accomplish specific tasks within your Gatsby sites.",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Learn how to add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-  },
-]
-
-const samplePageLinks = [
-  {
-    text: "Page 2",
-    url: "page-2",
-    badge: false,
-    description:
-      "A simple example of linking to another page within a Gatsby site",
-  },
-  { text: "TypeScript", url: "using-typescript" },
-  { text: "Server Side Rendering", url: "using-ssr" },
-  { text: "Deferred Static Generation", url: "using-dsg" },
-]
-
-const moreLinks = [
-  { text: "Join us on Discord", url: "https://gatsby.dev/discord" },
-  {
-    text: "Documentation",
-    url: "https://gatsbyjs.com/docs/",
-  },
-  {
-    text: "Starters",
-    url: "https://gatsbyjs.com/starters/",
-  },
-  {
-    text: "Showcase",
-    url: "https://gatsbyjs.com/showcase/",
-  },
-  {
-    text: "Contributing",
-    url: "https://www.gatsbyjs.com/contributing/",
-  },
-  { text: "Issues", url: "https://github.com/gatsbyjs/gatsby/issues" },
-]
-
-const utmParameters = `?utm_source=starter&utm_medium=start-page&utm_campaign=default-starter`
-
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <div className={styles.textCenter}>
-      <StaticImage
-        src="../images/example.png"
-        loading="eager"
-        width={64}
-        quality={95}
-        formats={["auto", "webp", "avif"]}
-        alt=""
-        style={{ marginBottom: `var(--space-3)` }}
-      />
-      <h1>
-        Welcome to <b>Gatsby!</b>
-      </h1>
-      <p className={styles.intro}>
-        <b>Example pages:</b>{" "}
-        {samplePageLinks.map((link, i) => (
-          <React.Fragment key={link.url}>
-            <Link to={link.url}>{link.text}</Link>
-            {i !== samplePageLinks.length - 1 && <> · </>}
-          </React.Fragment>
-        ))}
-        <br />
-        Edit <code>src/pages/index.js</code> to update this page.
-      </p>
-    </div>
-    <ul className={styles.list}>
-      {links.map(link => (
-        <li key={link.url} className={styles.listItem}>
-          <a
-            className={styles.listItemLink}
-            href={`${link.url}${utmParameters}`}
-          >
-            {link.text} ↗
-          </a>
-          <p className={styles.listItemDescription}>{link.description}</p>
-        </li>
-      ))}
-    </ul>
-    {moreLinks.map((link, i) => (
-      <React.Fragment key={link.url}>
-        <a href={`${link.url}${utmParameters}`}>{link.text}</a>
-        {i !== moreLinks.length - 1 && <> · </>}
-      </React.Fragment>
-    ))}
-  </Layout>
-)
-
-/**
- * Head export to define metadata for the page
- *
- * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
- */
-export const Head = () => <Seo title="Home" />
-
-export default IndexPage
+export default function HomePage() {
+    return (
+        <div>
+            <Jumbotron></Jumbotron>
+            <AboutMeComponent></AboutMeComponent>
+            <ExperienceSection></ExperienceSection>
+            <div class="container_4">
+            <div id = "section3" class="projects_line">
+            <StaticImage 
+            className = {"projects_image"}
+            src="C:\Users\shant\portfolio\gatsby-starter-default\src\components\Card\Ananya Maddali-1 (1).png"></StaticImage>&nbsp;&nbsp;&nbsp;&nbsp;
+            <h1 class="my_projects">My <span>&nbsp;Projects</span></h1>
+            </div>
+            <div class="cards_container">
+                {CardComponent}
+            </div>
+            </div>
+            <div class="container_5">
+            <div id = "section4" class="skills_section">
+            <div class="skills_line">
+            <StaticImage 
+            className = {"skills_image"}
+            src="C:\Users\shant\portfolio\gatsby-starter-default\src\components\SkillsGraph\Ananya Maddali-1 (2) (1).png"></StaticImage>&nbsp;&nbsp;&nbsp;&nbsp;
+                <h1 class="skill_heading">My <span>&nbsp;Skills</span></h1>
+                </div>
+                <div class="skill_columns">
+                {SkillsComponent}
+                </div>
+            </div>
+            </div>
+            <div class="container_4">
+            <div
+            id = "section5" variant = "outline">{ContactCard}</div>
+            </div>
+            <FooterComponent></FooterComponent>
+        </div>
+    )
+}
